58e2e4a1743fa1ad0c8b1a932477e2d7a38830b5
Benjaaaaaaaaaa <161783260+Benjaaaaaaaaaa@users.noreply.github.com>
2024-02-29 22:31:28 -0300

    Update meme_dict.py

--- pene_dict.py
+++ pene_dict.py
@@ -1,4 +1,5 @@
-pene_dict = {
+import random
+meme_dict = {
             "CRINGE": "Algo excepcionalmente raro o embarazoso",
             "LOL": "Una respuesta común a algo gracioso",
             "xd": "Risa",
@@ -9,8 +10,8 @@ pene_dict = {
 palabra= input("Que palabra no entiendes?")
 
 if palabra in pene_dict:
-    print(palabra+ "=", pene_dict[palabra])
+    print(palabra+ "=", meme_dict[palabra])
 else:
     print("no tengo la definicion de esa palabra")
     clave = random.choice(pene_dict.key() )
-    print(clave+ ":", pene_dict[clave])
+    print(clave+ ":", meme_dict[clave])
